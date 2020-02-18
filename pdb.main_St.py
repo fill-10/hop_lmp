ccd6f74bd412f9c387f1ee15907a103ec80e5db4
fill-10 <lawshuipok@hotmail.com>
2020-02-18 17:53:35 -0500

    Add files via upload

--- pdb.main_St.py
+++ pdb.main_St.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     import time as timer
     ##--- output general settings ---=
     import numpy as np
-    fn_prefix = '../AmC2_'
+    fn_prefix = '../Pf6C2_'
  
     ##--- timer start ---
     start = timer.perf_counter()
@@ -13,14 +13,15 @@ if __name__ == '__main__':
     d1 = data()
     d1.save_mem=1 # save atoms and ions
     ##--- read ht from file ---
-    d1.L_all_ht = np.loadtxt('./ht3.dat', dtype=int)
+    d1.L_all_ht = np.loadtxt('./ht1.dat', dtype=int)
     d1.L_all_ht = np.append(d1.L_all_ht,  np.loadtxt('./ht4.dat', dtype=int)[1:], axis=0)
-    #d1.L_all_ht = np.append(d1.L_all_ht,  np.loadtxt('./ht3.dat', dtype=int)[1:], axis=0)
-    #d1.L_all_ht = np.append(d1.L_all_ht,  np.loadtxt('./ht4.dat', dtype=int)[1:], axis=0)
+    d1.L_all_ht = np.append(d1.L_all_ht,  np.loadtxt('./ht3.dat', dtype=int)[1:], axis=0)
+    d1.L_all_ht = np.append(d1.L_all_ht,  np.loadtxt('./ht4.dat', dtype=int)[1:], axis=0)
     print(d1.L_all_ht.shape[0], ' * ', d1.L_all_ht.shape[1])
     ##--- St ---
-    time_col, St_col = d1.St( 10, 500 )
-    np.savetxt(    fn_prefix+'St3050.dat', np.transpose( [time_col, St_col] ), fmt=['%d', '%f'], header='time/ps, St'      )
+    time_col, St_col = d1.St( 10 )
+    np.savetxt(    fn_prefix+'St_new.dat', np.transpose( [time_col, St_col] ), fmt=['%d', '%f'], header='time/ps, St'      )
     ##--- timer stop ---
     stop = timer.perf_counter()
+    print('St calculated.')
     print('time used in sec: ', stop-start)
